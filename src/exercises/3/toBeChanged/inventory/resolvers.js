c0323923d7807e50f6285a20ccc406fd76aeeb43
lgandecki <lgandecki@css.edu>
2019-08-06 10:00:22 +0400

    improved explanation

--- src/exercises/3/toBeChanged/inventory/resolvers.js
+++ src/exercises/3/toBeChanged/inventory/resolvers.js
@@ -1,8 +1,8 @@
 exports.resolvers = {
   Product: {
     // 🐨 Add shippingEstimate resolver here. Use the provided estimateShipping function, and pass to it an object with price and weight.
-    // The first argument to the shippingEstimate function will be an object resolved by the __resolveReference function below.
-    // There is one more thing you will have to change in this file.
+    // The first argument to the shippingEstimate resolver function will be an object resolved by the __resolveReference function below.
+    // There is one more thing you will have to change in this file. Can you spot it?
 
     // 💰 you can start with this function. Do you have all the required data?
     // shippingEstimate: object => {
@@ -18,7 +18,7 @@ exports.resolvers = {
   }
 };
 
-// 💯 Move this helper to another file and write unit tests for it.
+// 💯 (extra credit) Move this helper to another file and write unit tests for it.
 // Think about testing the GraphQL layer. Take a look at the implementation of 1.test.js. How can you avoid this kind of coupling of your high-level tests to the details of the business logic for the shipping calculations?
 // 🦉 Your GraphQL tests should be independent of underlying business logic. In other words - if the rules for shipping changes, only the unit tests for estimateShipping should change.
 
